96f1a26e60db74b82014e0aec0eae1ffd7ca4aba
Eng.gamal <eng.gamalmegahed@gmail.com>
2024-12-29 22:08:31 +0200

    More adhocs

---
+++ Fun_Game.cpp
@@ -0,0 +1,55 @@
+// "ولا تقولن لشيء إني فاعل ذلك غدا"
+// "إلا أن يشاء الله واذكر ربك إذا نسيت وقل عسى أن يهديني ربي لأقرب من هذا رشدا"
+
+// LINK : https://codeforces.com/problemset/problem/1994/B
+#include <bits/stdc++.h>
+#define ll long long
+#define nl '\n'
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+#define no cout<<"NO\n"
+#define yes cout<<"YES\n"
+#define ENG_GAMAL ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+const int N = 2e5 + 5, M = 1e3, LOG = 20, inf = 0x3f3f3f3f;
+ll infLL = 0x3f3f3f3f3f3f3f3f;
+
+void solve() {
+    ll n;
+    string s , t;
+    cin >> n >> s >> t;
+    bool found_one = false;
+    for (ll i = 0; i < n; ++i) {
+        if(s[i] != t[i] && !found_one && s[i] == '0')
+        {
+                return void(no);
+        }
+        if(s[i] == '1')
+            found_one = true;
+    }
+    yes;
+}
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+}
+
+int main() {
+    file();
+    ENG_GAMAL
+// test-independent code ——————————————————————
+// ————————————————————————————————————————————
+    ll t = 1;
+     cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
