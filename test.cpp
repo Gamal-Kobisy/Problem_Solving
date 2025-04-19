28b1bdf664ffcb143cfc4c199e7f4b3dbdf91e9a
Eng.gamal <eng.gamalmegahed@gmail.com>
2025-04-20 00:38:06 +0200

    الحمد لله

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+
 // "ولا تقولن لشيء إني فاعل ذلك غدا"
 // "إلا أن يشاء الله واذكر ربك إذا نسيت وقل عسى أن يهديني ربي لأقرب من هذا رشدا"
 
@@ -5,50 +6,30 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define nl '\n'
+#define sp ' '
 #define all(a) a.begin(),a.end()
 #define allr(a) a.rbegin(),a.rend()
 #define no cout<<"NO\n"
 #define yes cout<<"YES\n"
 #define ENG_GAMAL ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 using namespace std;
-// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————— 
 const int N = 2e5 + 5, M = 1e3, LOG = 20, inf = 0x3f3f3f3f;
 ll infLL = 0x3f3f3f3f3f3f3f3f;
-double solve(double x) {
-    // تمت سرقتها من محمد و عبدو
-    if(x >= 1 && x <= 2)
-    {
-        return 1;
-    }
-    if(x >= 2)
-    {
-        return solve(x - 1) * (x - 1);
-    }
-    else
-    {
-        cout << (x + 1) << nl;
-        return solve(x + 1) * (x + 1);
-    }
-}
-void file()
-{
-#ifndef ONLINE_JUDGE
-    freopen("Input.txt", "r", stdin);
-    freopen("Output.txt", "w", stdout);
-    freopen("Error.txt", "w", stderr);
-#endif
+
+void solve() {
+	cout << "Hello World!" << nl;
 }
 
 int main() {
-    file();
     ENG_GAMAL
-// test-independent code ——————————————————————
-// ————————————————————————————————————————————
-    ll t = 1;
-    // cin >> t;
+        // test-independent code ——————————————————————
+        // ———————————————————————————————————————————— 
+        ll t = 1;
+    //     cin >> t;
     while (t--)
     {
-        cout << solve(-3.7);
+        solve();
     }
 
     return 0;
