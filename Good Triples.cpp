1309d5a535335c34e1454ca33c2ad3e3aa9146b7
Eng.gamal <eng.gamalmegahed@gmail.com>
2025-11-14 10:15:59 +0200

    1600 sheet

---
+++ Good Triples.cpp
@@ -0,0 +1,89 @@
+// "ولا تقولن لشيء إني فاعل ذلك غدا"
+// "إلا أن يشاء الله واذكر ربك إذا نسيت وقل عسى أن يهديني ربي لأقرب من هذا رشدا"
+
+// LINK : https://codeforces.com/problemset/problem/1907/E
+#pragma GCC optimize("O3")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize ("Ofast")
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PI acos(-1)
+#define Ones(n) __builtin_popcountll(n)
+#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+#define no cout<<"NO\n"
+#define yes cout<<"YES\n"
+#define imp cout<<"IMPOSSIBLE\n"
+#define nl '\n'
+#define sp ' '
+#define ENG_GAMAL ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+const int N = 2e5 + 5, M = 1e3, LOG = 20, inf = 0x3f3f3f3f;
+ll infLL = 0x3f3f3f3f3f3f3f3f;
+
+ll fact(int n) {
+    ll res = 1;
+    for (int i = 2; i <= n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+ll nCr(int n, int r) {
+    if (r > n || r < 0) return 0;
+    return fact(n)/ (fact(r) * fact(n - r));
+}
+
+ll nPr(int n, int r) {
+    if (r > n || r < 0) return 0;
+    return fact(n) / fact(n - r);
+}
+
+ll starsBars(int n , int k){
+    return nCr(n + k - 1 , k - 1);
+}
+
+void TC() {
+    int n;
+    cin >> n;
+    ll ans = 1;
+    while(n){
+       int dig = n % 10;
+       n /= 10;
+       ans *= starsBars(dig , 3);
+    }
+    cout << ans << nl;
+}
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+}
+
+int main() {
+    file();
+    ENG_GAMAL
+// test-independent code ——————————————————————
+// ————————————————————————————————————————————
+    ll tc = 1;
+     cin >> tc;
+    while (tc--)
+    {
+        TC();
+    }
+
+    return 0;
+}
---
+++ Partitioning the Array.cpp
@@ -0,0 +1,102 @@
+// "ولا تقولن لشيء إني فاعل ذلك غدا"
+// "إلا أن يشاء الله واذكر ربك إذا نسيت وقل عسى أن يهديني ربي لأقرب من هذا رشدا"
+
+// LINK : https://codeforces.com/problemset/problem/1920/C
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC optimize ("Ofast")
+#include <bits/stdc++.h>
+//#pragma GCC target("avx2")
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PI acos(-1)
+#define Ones(n) __builtin_popcountll(n)
+#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+#define no cout<<"NO\n"
+#define yes cout<<"YES\n"
+#define imp cout<<"IMPOSSIBLE\n"
+#define nl '\n'
+#define sp ' '
+#define ENG_GAMAL ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+const int N = 2e5 + 5, M = 1e3, LOG = 20, inf = 0x3f3f3f3f;
+ll infLL = 0x3f3f3f3f3f3f3f3f;
+
+ll gcd(ll a, ll b) {
+    while (b != 0) {
+        ll temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+ll gcd_vector(const vector<ll>& vec) {
+    ll result = vec[0];
+    for (size_t i = 1; i < vec.size(); ++i) {
+        result = gcd(result, vec[i]);
+        if (result == 1) {
+            return 1;
+        }
+    }
+    return result;
+}
+
+void TC() {
+    int n;
+    cin >> n;
+    vector<int>a(n),v(n),divs;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    v  = a;
+    for(ll i = 1 ; i * i <= n ; i++){
+        if(n % i) continue;
+        divs.pb(i);
+        if(i != n / i) divs.pb(n / i);
+    }
+    for (int i = 1; i < n; ++i) {
+        v[i] = abs(a[i] - a[i - 1]);
+    }
+    int ans = 0;
+    for(int div : divs){
+        int curGcd = 0;
+        for (int i = 0; i + div < n; i++) {
+            curGcd = gcd(curGcd, abs(a[i + div] - a[i]));
+        }
+        ans += (curGcd != 1);
+    }
+    cout << ans << nl;
+}
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+}
+
+int main() {
+    file();
+    ENG_GAMAL
+// test-independent code ——————————————————————
+// ————————————————————————————————————————————
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        TC();
+    }
+
+    return 0;
+}
