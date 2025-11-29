73fd412f1b8adbbf7cfbba90fbf57b8d91fd1d6b
Eng.gamal <eng.gamalmegahed@gmail.com>
2025-11-29 23:57:14 +0200

    more adhocs

---
+++ Split.cpp
@@ -0,0 +1,89 @@
+// "ولا تقولن لشيء إني فاعل ذلك غدا"
+// "إلا أن يشاء الله واذكر ربك إذا نسيت وقل عسى أن يهديني ربي لأقرب من هذا رشدا"
+
+// LINK : https://codeforces.com/contest/2158/customtest
+#pragma GCC optimize("O3")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize ("Ofast")
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PI acos(-1)
+#define Ones(n) __builtin_popcountll(n)
+#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+#define no cout<<"NO\n"
+#define yes cout<<"YES\n"
+#define imp cout<<"IMPOSSIBLE\n"
+#define nl '\n'
+#define sp ' '
+#define ENG_GAMAL ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+const int N = 2e5 + 5, M = 1e3, LOG = 20, inf = 0x3f3f3f3f;
+ll infLL = 0x3f3f3f3f3f3f3f3f;
+
+void TC() {
+    int n;
+    cin >> n;
+    int total_len = n * 2;
+    map<int, int> freq;
+
+    for (int i = 0; i < total_len; ++i) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    int cnt_odd = 0;
+    int cnt_even = 0;
+
+    for(auto [x, f] : freq){
+        if(f % 2 != 0) {
+            cnt_odd++;
+        } else {
+            cnt_even++;
+        }
+    }
+    int ans = cnt_odd + 2 * cnt_even;
+
+    if (cnt_odd == 0) {
+        if (cnt_even % 2 != n % 2) {
+            ans -= 2;
+        }
+    }
+
+    cout << ans << nl;
+}
+
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+}
+
+int main() {
+    file();
+    ENG_GAMAL
+// test-independent code ——————————————————————
+// ————————————————————————————————————————————
+    ll tc = 1;
+     cin >> tc;
+    while (tc--)
+    {
+        TC();
+    }
+
+    return 0;
+}
