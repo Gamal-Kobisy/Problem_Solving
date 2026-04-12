a0dd664297c511ed5a274746bf5042458f54fc41
Eng.gamal <eng.gamalmegahed@gmail.com>
2026-04-12 09:30:04 +0200

    organizing the files

--- Say Hello With C++.cpp
+++
@@ -1,9 +0,0 @@
-// LINK : https://vjudge.net/problem/Gym-287306A
-#include <iostream>
-using namespace std;
-int main() {
-    string name;
-    cin>>name;
-    cout<<"Hello, "<<name<<"\n";
-    return 0;
-}
